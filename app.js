323a8a63c5f622ddf47c7ecd1204a05541afdeff
Nate Fitch <nfitch@nfprod.com>
2022-12-08 21:58:21 -0800

    Serving hello world from static directory.

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 #!/usr/bin/env node
 
-console.log("Hello world!");
+const config = {
+    "port": 8080
+};
+const path = require('path');
+
+// Server, setup serving from ./static
+const fastify = require('fastify')({
+    logger: true
+});
+fastify.register(require('@fastify/static'), {
+    root: path.join(__dirname, 'static')
+})
+
+// List app routes
+fastify.get('/list', function(request, reply) {
+    reply.send({ hello: 'world' })
+});
+
+// Run the server!
+fastify.listen(config, function(err, address) {
+    if (err) throw err;
+    // Server is now listening on ${address}
+});
